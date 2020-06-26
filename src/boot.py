a7a4c786eab63e3d9d7577d3f9a0b601cdf5ef6d
luisvilaca9 <luisvilaca9@gmail.com>
2020-06-26 20:56:38 +0100

    master

--- src/boot.py
+++ src/boot.py
@@ -2,13 +2,13 @@ from secrets import ssid, password
 import network
 
 def do_connect():
-  wlan = network.WLAN(network.STA_IF)
-  wlan.active(True)
-  if not wlan.isconnected():
-    print('connecting to network...')
-    wlan.connect(ssid, password)
+    wlan = network.WLAN(network.STA_IF)
+    wlan.active(True)
+    if not wlan.isconnected():
+        print('connecting to network...')
+        wlan.connect(ssid, password)
     while not wlan.isconnected():
-      pass
-  print('network config:', wlan.ifconfig())
+        pass
+    print('network config:', wlan.ifconfig())
 
 do_connect()
